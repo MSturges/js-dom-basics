4684a0a2828c4539e7e70051032cc003ec26087a
MSturges <Sturges3232@gmail.com>
2016-02-15 15:29:44 -0700

    Dom practice

--- main.js
+++ main.js
@@ -1,1 +1,64 @@
-console.log("sanity check!")
+//test
+// console.log("sanity check!")
+//
+// Assigns the firstParagraph to the id of main in js.
+// var firstParagraph = document.getElementById('main');
+// console.log(firstParagraph);
+
+
+// var getAllElements = document.getElementsByTagName('ul');
+// console.log(getAllElements)
+//
+// var getSingleElement = document.getElementsByTagName('ul')[0];
+// console.log(getSingleElement)
+
+// allListItems = document.getElementsByTagName('li');
+// console.log(allListItems);
+//
+// for (var i = 0; i < allListItems.length; i++) {
+//   console.log(allListItems[i]);
+// }
+
+
+// var evenElements = document.getElementsByClassName('even');
+// console.log(evenElements)
+//
+// var oddElements = document.getElementsByClassName('odd');
+// console.log(oddElements)
+//
+// var firstEvenElement = document.getElementsByClassName('even')[0];
+// console.log(firstEvenElement)
+//
+// var firstOddElement = document.getElementsByClassName('odd')[0];
+// console.log(firstOddElement)
+
+
+// var main = document.querySelector('#main');
+// console.log(main);
+
+// var firstOddElement = document.querySelector('.odd');
+// console.log(firstOddElement);
+
+// var oddAndEvenElements = document.querySelectorAll('.odd, .even');
+// console.log(oddAndEvenElements);
+
+// for (var i = 0; i < allListItems.length; i++) {
+//   console.log(allListItems[i].innerText);
+// }
+//
+// for (var i = 0; i < allListItems.length; i++) {
+//   console.log(allListItems[i].innerText = i);
+// }
+// console.log(allListItems);
+
+// document.getElementById('main').style.backgroundColor = "goldenrod";
+
+// var li = document.createElement('li');
+// console.log(li);
+//
+// var newElement = li.innerText = "another list item";
+// console.log(newElement);
+// console.log(li);
+//
+// getSingleElement.appendChild(li);
+// console.log(allListItems);
